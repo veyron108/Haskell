f4cb25a96ec57827c1d463eeeda9c08783cc7146
Fabian Ljubanovic <veyron108@gmail.com>
2022-07-19 22:34:46 +1000

    latest

--- clock6/src/Main.hs
+++ clock6/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Concurrent
+import Control.Monad.IO.Class
 import Control.Monad.Trans.State
 import Control.Monad
 import Data.List
@@ -33,10 +34,12 @@ data Event = SecondsEvent | KeyEvent Char deriving Show
 utcToUtc :: Int -> UTCTime -> UTCTime
 utcToUtc offset = addUTCTime (realToFrac offset)                          -- changing this value is working fine
 
+
 stringToInt :: String -> Int
 stringToInt x = if all (`elem` "0123456789") x
                 then read x :: Int
                 else 0
+
 
 splitTime :: String -> [String]
 splitTime [] = [""]
@@ -44,52 +47,64 @@ splitTime (x : xs) | x == ':'  = "" : more
              | otherwise = (x : head more) : tail more
     where more = splitTime xs
 
+
 -------------------------------------------------------
--- Clock State
+-- IO Monad
 --
+io :: MonadIO m => IO a -> m a
+io = liftIO
+
+
 getDayTime :: Int -> IO DayTime
 getDayTime n = do
-    now <- getCurrentTime                                     -- eg 2022-07-13 17:25:29.4547484 ->UTC
-    timezone <- getCurrentTimeZone                            -- eg AEST
-    let now'      = utcToUtc n now                            -- changed the offset
-    let zoneNow   = utcToLocalTime timezone now'              -- eg 2022-07-14 03:24:41.7410000 ->Melb
-    let timeOfDay = formatTime defaultTimeLocale "%H:%M:%S" zoneNow 
-    let [h, m, s] = splitTime timeOfDay
-    let hours     = stringToInt h
-    let minutes   = stringToInt m
-    let seconds   = stringToInt s
-    return (hours, minutes, seconds)
+  now <- getCurrentTime                                      -- eg 2022-07-13 17:25:29.4547484 ->UTC
+  timezone <- getCurrentTimeZone                             -- eg AEST
+  let now'      = utcToUtc n now                                -- changed the offset
+  let zoneNow   = utcToLocalTime timezone now'                  -- eg 2022-07-14 03:24:41.7410000 ->Melb
+  let timeOfDay = formatTime defaultTimeLocale "%H:%M:%S" zoneNow 
+  let [h, m, s] = splitTime timeOfDay
+  let hrs   = stringToInt h
+  let min   = stringToInt m
+  let sec   = stringToInt s
+  return (hrs, min, sec)
+
 
 initClockState :: IO ClockState 
 initClockState = do 
   dt <- getDayTime 0
   return $ ClockState { timeOfDay = dt, offset = 0 }
 
-getAwst :: State Int String
-getAwst = do                            -- AWST
-    current <- get                      -- Get the current state (our offset)
-    put (- 7200)                        -- Update the state 
-    return (show current)               -- Produce result
 
-getAcst :: State Int String
-getAcst = do                            -- ACST
-    current <- get                      -- 
-    put (- 1800)                        --  
-    return (show current)               -- 
+-- NOT USED
+-- getAwst :: State Int String
+-- getAwst = do                            -- AWST
+--     current <- get                      -- Get the current state (our offset)
+--     put (- 7200)                        -- Update the state 
+--     return (show current)               -- Produce result
 
-getAest :: State Int String
-getAest = do                            -- AEST 
-    current <- get                      -- 
-    put 0                               -- 
-    return (show current)               -- 
+-- NOT USED
+-- getAcst :: State Int String
+-- getAcst = do                            -- ACST
+--     current <- get                      -- 
+--     put (- 1800)                        --  
+--     return (show current)               -- 
 
-getClockStateOffset :: ClockState -> Int
-getClockStateOffset ClockState { timeOfDay = b, offset = c } = c
+-- NOT USED
+-- getAest :: State Int String
+-- getAest = do                            -- AEST 
+--     current <- get                      -- 
+--     put 0                               -- 
+--     return (show current)               -- 
+
+-- NOT USED
+-- getClockStateOffset :: ClockState -> Int
+-- getClockStateOffset ClockState { timeOfDay = b, offset = c } = c
 
 -------------------------------------------------------
 -- Clock Rendering
 --
-drawVAnim :: Int -> [String] -> [String]                                  -- NOT IN USE
+-- NOT USED
+drawVAnim :: Int -> [String] -> [String]                               
 drawVAnim 0 [] = []
 drawVAnim n (x : xs) 
   | n > 0 = x : drawVAnim (n - 1) xs 
@@ -107,7 +122,7 @@ convertNestedListToString n = concat $ concat n
 -- Manipulate the List of Strings so they appear horizontally by transposing
 -- Notes: Changed from own function to using transpose - forgot that was available earlier - doh!
 
--- NOT IN USE
+-- NOT USED
 -- colToRows :: [[a]] -> [[a]]
 -- colToRows ([]:_) = []
 -- colToRows r = map head r : colToRows (map tail r)
@@ -127,7 +142,7 @@ drawClockState :: State ClockState String
 drawClockState = do
     cs <- get
     let os = offset cs
-    let dt = timeOfDay cs
+    let dt = timeOfDay cs     --getDayTime os -- change to this
     put(cs{timeOfDay = dt, offset = os})
     return $ drawClock dt
 
@@ -172,16 +187,15 @@ main = do
   cs <- initClockState                        -- create initial ClockState instance
 
   setCursorPosition 0 0
-  --print ics 
 
-  -- NOT IN USE : Heading Animation
+  -- NOT USED : Heading Animation
   -- forM_ [1..8] (\ i -> do                                            
   --   setCursorPosition 0 0 
   --   threadDelay (10 ^ 5)
   --   putStrLn $ concat $ drawVAnim i title
   --   )
 
-  chan <- newChan               -- Channels and Events - see last class with fork.hs
+  chan <- newChan                             -- Channels and Events - see last class with fork.hs
   forkIO $ ticker chan 
   forkIO $ input chan
 
@@ -192,9 +206,10 @@ main = do
       -------- TICKER --------
       SecondsEvent  -> do
 
-        let os = offset cs            -- "get" offset value out of ClockState instance
-                                      -- Note: if I manually type a value here it works       
         setCursorPosition 0 0
+        
+        let os = offset cs                    -- "get" offset value out of ClockState instance
+                                              -- Note: if I manually type a value here it works       
         dt <- getDayTime os
         putStrLn $ evalState drawClockState cs{ timeOfDay = dt, offset = os }
 
@@ -217,37 +232,29 @@ main = do
           clearFromCursorToScreenEnd
           exitFailure
 
-        'e' -> do 
-          -- "set" offset in ClockState to 0
+        'e' -> do                             -- "set" offset in ClockState to 0 
           setCursorPosition 0 0
           let os = 0 :: Seconds
           dt <- getDayTime os
           putStrLn $ evalState drawClockState cs{ timeOfDay = dt, offset = os }
-          return ()
           
-        'w' -> do 
-          -- "set" offset in ClockState to (-7200)
+        'w' -> do                             -- "set" offset in ClockState to (-7200)
           setCursorPosition 0 0
           let os = (-7200) :: Seconds
           dt <- getDayTime os
           putStrLn $ evalState drawClockState cs{ timeOfDay = dt, offset = os }
-          return ()
 
-        'c' -> do 
-          -- "set" offset in ClockState to (-1800)
+        'c' -> do                             -- "set" offset in ClockState to (-1800)
           setCursorPosition 0 0
           let os = (-1800) :: Seconds
           dt <- getDayTime os
           putStrLn $ evalState drawClockState cs{ timeOfDay = dt, offset = os }
-          return ()
 
-        'a' -> do 
-          -- "set" offset in ClockState to (43200) - Pacific Auckland NZST
+        'a' -> do                             -- "set" offset in ClockState to (43200) - Pacific Auckland NZST
           setCursorPosition 0 0
           let os = 43200 :: Seconds
           dt <- getDayTime os
           putStrLn $ evalState drawClockState cs{ timeOfDay = dt, offset = os }
-          return ()
 
         _   -> do 
           return ()
